1002c197c10e90c0476308dff08880446425c6c6
Sohrab Ferdowsi <sohrab.ferdowsi@gmail.com>
2025-03-14 09:34:57 +0100

    Add repo init basics

---
+++ radiclef/__init__.py
@@ -0,0 +1,4 @@
+from .constants import REPO_DIR
+from .constants import CORPORA_DIR
+from .constants import ROCO_DATABASE_DOWNLOAD_PATH
+from .constants import RESOURCES_DIR
---
+++ radiclef/constants.py
@@ -0,0 +1,22 @@
+import os
+import socket
+
+
+REPO_DIR = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+REPO_NAME = os.path.split(REPO_DIR)[-1]
+
+datasets_root_dirs_dict = {
+    "unige-poc": "/data/",
+    "ordnance": "/data/bases/corpora/",
+    "sf-macbook-pro.local": "/Users/sssohrab/data/corpora"
+}
+
+hostname = socket.gethostname().lower()
+if hostname in datasets_root_dirs_dict.keys():
+    CORPORA_DIR = datasets_root_dirs_dict[hostname]
+else:
+    raise ValueError("The root data directory not assigned for the hostname.")
+
+ROCO_DATABASE_DOWNLOAD_PATH = os.path.join(CORPORA_DIR, "ROCOv2-radiology")
+
+RESOURCES_DIR = os.path.join(REPO_DIR, "resources")
