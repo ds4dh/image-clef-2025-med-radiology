a51edd1e9359ee23c744df07054cad5066166c34
Sohrab Ferdowsi <sohrab.ferdowsi@gmail.com>
2025-04-25 15:26:44 +0200

    Extend inference for CLEF-set without test-meta
    to choose between ROCO or CLEF, and to account for the unavailability of
    the challenge's test set.

--- radiclef/experiments/conv_to_cui_seq/inference.py
+++ radiclef/experiments/conv_to_cui_seq/inference.py
@@ -1,7 +1,7 @@
 import datasets
 import pandas as pd
 
-from radiclef import RESOURCES_DIR, ROCO_DATABASE_PATH
+from radiclef import RESOURCES_DIR, ROCO_DATABASE_PATH, CLEF_2025_DATABASE_PATH
 from radiclef.utils import ConceptUniqueIdentifiers, ImagePrepare
 from radiclef.networks import ConvEmbeddingToSec
 from radiclef.umls_api import CONCEPT_MAP_PATH
@@ -16,10 +16,18 @@ from datasets import load_from_disk
 import os
 import json
 
-RUN_TAG = "2025-04-09_07-29-28_unige-poc"
-DEVICE_NAME = "cuda:0"
-BATCH_SIZE = 50
+DATABASE_NAME = "CLEF"
+RUN_TAG = "2025-04-13_10-18-45_unige-poc"
+DEVICE_NAME = "mps"
+BATCH_SIZE = 100
 BEAM_SEARCH_WIDTH: None | int = 3
+
+if DATABASE_NAME == "ROCO":
+    DATABASE_PATH = ROCO_DATABASE_PATH
+elif DATABASE_NAME == "CLEF":
+    DATABASE_PATH = CLEF_2025_DATABASE_PATH
+else:
+    raise NotImplementedError
 
 run_dir = os.path.join("./runs", RUN_TAG)
 
@@ -32,7 +40,7 @@ network = load_network_from_state_dict_to_device(network, os.path.join(run_dir, 
 
 network = network.eval()
 
-dataset_dict = load_from_disk(ROCO_DATABASE_PATH)
+dataset_dict = load_from_disk(DATABASE_PATH)
 
 with open(os.path.join(RESOURCES_DIR, "cui-alphabet.txt"), "r") as f:
     cui_alphabet = [v.strip() for v in f.readlines()]
@@ -48,25 +56,37 @@ image_prep = ImagePrepare(standard_image_size=(config["data"]["image_size"][0], 
 
 def map_fields(example):
     image_tensor = torch.cat([image_prep(_img).unsqueeze(0) for _img in example["image"]], dim=0)
-    cui_seq = pad_sequence([torch.tensor(cui_object.encode_as_seq(_c)) for _c in example["cui_codes"]],
-                           batch_first=True, padding_value=cui_object.c2i[cui_object.PAD_TOKEN])
-    return {
+    output = {
         "image_tensor": image_tensor,
-        "cui_seq": cui_seq
     }
+    if "cui_codes" in example.keys():
+        cui_seq = pad_sequence([torch.tensor(cui_object.encode_as_seq(_c)) for _c in example["cui_codes"]],
+                               batch_first=True, padding_value=cui_object.c2i[cui_object.PAD_TOKEN])
+
+        output = {**output, **{
+            "cui_seq": cui_seq
+        }}
+
+    return output
 
 
 def eval_dataset(dataset: datasets.Dataset) -> pd.DataFrame:
+    is_metadata_available: bool
+    if "cui" in dataset.column_names:
+        is_metadata_available = True
+        header = ["ground-truth-codes", "predicted-codes", "ground-truth-concepts", "predicted-concepts", "f1-score"]
+    else:
+        is_metadata_available = False
+        header = ["predicted-codes", "predicted-concepts"]
+
     metric = F1Metric()
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
-    header = ["ground-truth-codes", "predicted-codes", "ground-truth-concepts", "predicted-concepts", "f1-score"]
     data = []
     idx_minibatch = -1
     for mini_batch in dataloader:
         idx_minibatch += 1
         print("Processing batch {}/{}".format(idx_minibatch + 1, len(dataloader)))
         im_tensor = mini_batch["image_tensor"].to(torch.device(DEVICE_NAME))
-        ground_truth_seq = mini_batch["cui_seq"].to(torch.device(DEVICE_NAME))
 
         with torch.no_grad():
             prediction_seq = network.predict(im_tensor,
@@ -75,19 +95,29 @@ def eval_dataset(dataset: datasets.Dataset) -> pd.DataFrame:
                                              max_len=32,
                                              beam_search_width=BEAM_SEARCH_WIDTH)
 
-        for idx in range(im_tensor.shape[0]):
-            _gt_seq = ground_truth_seq[idx, :]
-            _p_seq = prediction_seq[idx, :]
+        if is_metadata_available:
+            ground_truth_seq = mini_batch["cui_seq"].to(torch.device(DEVICE_NAME))
+            for idx in range(im_tensor.shape[0]):
+                _gt_seq = ground_truth_seq[idx, :]
+                _p_seq = prediction_seq[idx, :]
 
-            _data = [
-                cui_object.decode(_gt_seq.tolist()),
-                cui_object.decode(_p_seq.tolist()),
-                cui_object.decode_mapped(_gt_seq.tolist()),
-                cui_object.decode_mapped(_p_seq.tolist()),
-                metric.f1_score(ground_truth_seq=_gt_seq.unsqueeze(0), prediction_seq=_p_seq.unsqueeze(0))
-            ]
+                _data = [
+                    cui_object.decode(_gt_seq.tolist()),
+                    cui_object.decode(_p_seq.tolist()),
+                    cui_object.decode_mapped(_gt_seq.tolist()),
+                    cui_object.decode_mapped(_p_seq.tolist()),
+                    metric.f1_score(ground_truth_seq=_gt_seq.unsqueeze(0), prediction_seq=_p_seq.unsqueeze(0))
+                ]
+                data.append(_data)
 
-            data.append(_data)
+        else:
+            for idx in range(im_tensor.shape[0]):
+                _p_seq = prediction_seq[idx, :]
+                _data = [
+                    cui_object.decode(_p_seq.tolist()),
+                    cui_object.decode_mapped(_p_seq.tolist()),
+                ]
+                data.append(_data)
 
     return pd.DataFrame(data=data, columns=header, index=None)
 
@@ -101,7 +131,11 @@ if __name__ == "__main__":
         ds.set_transform(lambda x: map_fields(x))
         df = eval_dataset(ds)
         df.insert(0, 'image-ID', pd.Series(image_ids))
-        df_path = os.path.join(run_dir, "inference-{}_f1-score-{}.csv".format(
-            split, df["f1-score"].mean().item()))
+        if "f1-score" in df.columns:
+            df_path = os.path.join(run_dir, "{}-inference-{}_f1-score-{}.csv".format(
+                DATABASE_NAME, split, df["f1-score"].mean().item()))
+        else:
+            df_path = os.path.join(run_dir, "{}-raw-inference-{}.csv".format(
+                DATABASE_NAME, split))
 
         df.to_csv(df_path, index=False)
