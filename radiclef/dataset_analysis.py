9e0ff62444ecddf4831edc47558ee15e5e340ad7
Sohrab Ferdowsi <sohrab.ferdowsi@gmail.com>
2025-05-07 14:52:02 +0200

    Add label smoothing to the CE loss

--- radiclef/dataset_analysis.py
+++ radiclef/dataset_analysis.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-from radiclef import ROCO_DATABASE_PATH, RESOURCES_DIR
+from radiclef import CLEF_2025_DATABASE_PATH, RESOURCES_DIR
 
 from datasets import load_from_disk
 from typing import Dict
@@ -8,7 +8,7 @@ from typing import Dict
 from matplotlib.pyplot import plot
 import os
 
-dataset_dict = load_from_disk(ROCO_DATABASE_PATH)
+dataset_dict = load_from_disk(CLEF_2025_DATABASE_PATH)
 
 dataset_train = dataset_dict["train"].remove_columns(["image"])
 dataset_valid = dataset_dict["valid"].remove_columns(["image"])
@@ -27,17 +27,11 @@ for idx, cui_list in enumerate(dataset_valid["cui_codes"]):
     for cui in cui_list:
         cui_frequency_dict_valid[cui] += 1 / len(dataset_valid)
 
-cui_frequency_dict_test = {cui: 0 for cui in cui_alphabet}
-for idx, cui_list in enumerate(dataset_test["cui_codes"]):
-    for cui in cui_list:
-        cui_frequency_dict_test[cui] += 1 / len(dataset_test)
 
 print(cui_frequency_dict_train)
 print(cui_frequency_dict_valid)
-print(cui_frequency_dict_test)
 
 plt.figure()
 plot([v for v in cui_frequency_dict_train.values()])
 plot([v for v in cui_frequency_dict_valid.values()])
-plot([v for v in cui_frequency_dict_test.values()])
 plt.show()
